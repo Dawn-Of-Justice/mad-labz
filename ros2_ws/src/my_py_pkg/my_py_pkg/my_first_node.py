5f2870e846c02f390fd1acd523ae4b4dc30c19f7
Salo Soja Edwin <salosoja@gmail.com>
2024-02-28 19:24:24 +0530

    turtlesim-learn

--- ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
+++ ros2_ws/src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,7 +7,7 @@ class MyNode(Node):
     def __init__(self):
         super().__init__("py_test")
         self.counter_ = 0
-        self.get_logger().info("Hello ROS2")
+        self.get_logger().info("Hello ROS2!!!")
         self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
